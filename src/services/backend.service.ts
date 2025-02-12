54e728d2c3b10871d224bbd64fea450c9f671a6b
Tobias Kussel <tobias.kussel@dkfz-heidelberg.de>
2025-02-12 09:43:43 +0100

    chore: switch main branch to production backend

--- src/services/backend.service.ts
+++ src/services/backend.service.ts
@@ -27,8 +27,7 @@ export const requestBackend = (
 	if (import.meta.env.VITE_TARGET_ENVIRONMENT === "production") {
 	    backendUrl = "https://explorer.eucaim.cancerimage.eu/backend/";
 	} else if (import.meta.env.VITE_TARGET_ENVIRONMENT === "staging") {
-		// TODO: Change back to test instance after merge to main
-		backendUrl = 'https://explorer-eucaim.grycap.i3m.upv.es/backend/';
+	  backendUrl = "https://explorer.eucaim.cancerimage.eu/backend/";
 	} else {
 		backendUrl = 'http://localhost:8055';
 	}
