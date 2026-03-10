ab6505ba0247e32fb873fd639546eab60ebd2a69
Klein <paola.klein@dkfz-heidelberg.de>
2026-03-10 15:30:46 +0100

    fix: use target environment

--- src/config/options.ts
+++ src/config/options.ts
@@ -1,16 +1,9 @@
-import { env } from '$env/dynamic/public';
 import type { LensOptions } from '@samply/lens';
 import optionsProd from '../config/options.json';
 import optionsTest from '../config/options-test.json';
 
-console.log('Environment:', env.PUBLIC_ENVIRONMENT);
-env.PUBLIC_ENVIRONMENT = 'test';
-//env.PUBLIC_ENVIRONMENT = 'production';
-
+console.log('Environment:', import.meta.env.VITE_TARGET_ENVIRONMENT);
 export let options: LensOptions = optionsProd;
-if (env.PUBLIC_ENVIRONMENT === 'test') {
+if (import.meta.env.VITE_TARGET_ENVIRONMENT === 'staging') {
 	options = optionsTest;
 }
-/* if (env.PUBLIC_SPOT_URL) {
-            options.spotUrl = env.PUBLIC_SPOT_URL;
-        }*/
