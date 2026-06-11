d1df8e36c8b06c50b4ae7367cbf173d9e9a8d498
Tobias Kussel <TKussel@users.noreply.github.com>
2026-06-11 16:48:45 +0200

    Empty local resultStore on new request

--- src/services/backend.service.ts
+++ src/services/backend.service.ts
@@ -24,6 +24,7 @@ export const callBackend = async () => {
 	abortController.abort();
 	abortController = new AbortController();
 	clearSiteResults();
+	resultsStore.set([]); // The backend service maintains a local resultStore that needs to be emptied, too
 
 	/** Helper function to base64 encode a UTF-8 string */
 	const base64Encode = (utf8String: string) =>
